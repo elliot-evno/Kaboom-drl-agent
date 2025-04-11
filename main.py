78d36e019c5d837a505278540db95ec59609bd31
elliot <elliot@norrevik.se>
2025-04-11 18:20:48 +0200

    seperate dqn file

--- main.py
+++ main.py
@@ -9,18 +9,8 @@ import torch.nn as nn
 import torch.optim as optim
 import random
 from collections import deque
+from dqn import DQN
 
-class DQN(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, output_size)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
 
 class KaboomEnv:
     def __init__(self, width=800, height=600, basket_width=100):
